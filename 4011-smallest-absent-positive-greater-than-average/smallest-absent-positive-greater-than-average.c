e262db5d3a66a5dc50d429db7485e9d6d19a0982
Dha143 <k20950528@gmail.com>
2026-05-18 16:28:25 +0530

    Time: 3 ms (25.00%) | Memory: 12.7 MB (25.00%) - LeetSync

---
+++ 4011-smallest-absent-positive-greater-than-average/smallest-absent-positive-greater-than-average.c
@@ -0,0 +1,31 @@
+int smallestAbsent(int* nums, int numsSize) {
+int i,s=0,j,f,max;
+float avg;
+for(i=0;i<numsSize;i++)
+s=s+nums[i];
+avg=(float)s/numsSize;
+max=nums[0];
+for(i=1;i<numsSize;i++)
+if(nums[i]>max)
+max=nums[i];
+if(max<0)
+max=0;
+for(i=1;i<=max+1;i++)
+{
+if((float)i>avg)
+f=0;
+if(f==0)
+for(j=0;j<numsSize;j++)
+if(i==nums[j])
+{
+f=1;
+break;
+}
+if(f==0)
+{
+return i;
+break;
+}
+}
+return 0;
+}
